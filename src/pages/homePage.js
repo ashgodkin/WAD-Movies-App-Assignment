3ce8c947f0a2774a3ddaac4f060b8c622b0f130e
ashgodkin <ashgodkin@hotmail.com>
2023-06-27 11:56:25 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -23,6 +23,14 @@ const MovieListPage = (props) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
   };
+
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -51,7 +59,7 @@ const MovieListPage = (props) => {
             genreFilter={genreFilter} 
           />
         </Grid>
-        <MovieList movies={displayedMovies}></MovieList>
+        <MovieList movies={displayedMovies} selectFavourite={addToFavourites} />
       </Grid>
     </Grid>
   );
