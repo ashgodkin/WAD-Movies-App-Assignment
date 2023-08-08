598b43544470c6e5ff6bd3596e9b76ac221b4be4
ashgodkin <ashgodkin@hotmail.com>
2023-08-08 15:52:58 +0100

    Using react-query to manage the brower-based cache.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,26 @@ import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import {Link} from 'react-router-dom'
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from "./components/siteHeader";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 const App = () => {
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <SiteHeader /> {/*New Header */}
       <ul>
@@ -17,17 +31,24 @@ const App = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
+          <Link to="/movies/upcoming">Upcoming</Link>
+        </li>
+        <li>
           <Link to="/movies/favourites">Favourites</Link>
         </li>
       </ul>
       <Routes>
-        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route exact path="/movies/upcoming" component={ <UpcomingMoviesPage /> } />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
     </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false}
+    />
+    </QueryClientProvider>
   );
 };
 
