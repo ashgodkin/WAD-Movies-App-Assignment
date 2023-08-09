9d247052c3c070e43f73a41da5721b4198897e47
ashgodkin <ashgodkin@hotmail.com>
2023-08-09 16:10:45 +0100

    Fix broken stories in Storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,13 @@ module.exports = {
   "stories": [
     "../src/**/*.stories.js"
   ],
+
   "addons": [
     "@storybook/addon-links",
-  ]
+  ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  }
 }
